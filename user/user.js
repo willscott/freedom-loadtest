06375c4bf586c14d1781a3082625c64deca12bea
Will Scott <willscott@gmail.com>
2014-04-20 19:45:56 -0700

    sketch of what user needs to do

--- user/user.js
+++ user/user.js
@@ -1,3 +1,32 @@
-freedom.on('push', function(n) {
-  freedom.emit('result', 'pushed to ' + n.n + ' in ' + 0);
+var social = freedom.social();
+var storage = freedom.storage();
+var myId;
+
+var myLog = "";
+
+freedom.on('create', function(id) {
+  if (!myId) {
+    myId = id;
+    social.login({
+      agent: myId
+    });
+    storage.get(myId).then(function(log) {
+      myLog = log;
+    });
+  } else {
+    console.error('Already a user!');
+  }
+});
+
+freedom.on('push', function(ids) {
+  var who = ids.split(",");
+  for (var i = 0; i < who.length; i++) {
+    social.sendMessage(who[i], "Push [" + i + "/" + who.length + "] to " + who[i] );
+  }
+});
+
+social.on('onMessage', function(msg) {
+  var str = JSON.stringify(msg);
+  myLog += str.length + "." + str;
+  storage.set(myId, myLog)
 });

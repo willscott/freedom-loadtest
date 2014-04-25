b60b23bcfec9418485bfe21d5ab4d938ab3e3d78
Will Scott <willscott@gmail.com>
2014-04-24 19:18:04 -0700

    user can be created by manager

--- pool.js
+++ pool.js
@@ -1,12 +1,19 @@
 //Todo: this is abusing the fact that the fdom namespace isn't well cleaned up in the node version.
 
 var Pool = function(caller, dispatch) {
-  console.warn('pool instantiated');
+  this.caller = caller;
+  this.policy = this.caller.config.policy;
+  this.core = this.caller.core;
 };
 
 Pool.prototype.setup = function(id, continuation) {
-  console.warn('pool asked to make user container & bind with ' + id);
-  continuation('hi');
+  var base = this.caller.manifestId;
+  fdom.resources.get(base, 'user/user.json').then(function(url) {
+    this.policy.get(this.caller.lineage, url).then(function(user) {
+      this.core.manager.setup(user);
+      this.core.bindChannel(id, continuation, user);
+    }.bind(this));
+  }.bind(this));
 };
 
 fdom.apis.register('core.echo', Pool);
